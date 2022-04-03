ab14b31c304a620b46a2fe53cdb58443144deca2
yuvapriyakrr <yuvapriyakrr@gmail.com>
2022-04-03 11:42:59 +0530

    number pattern added

---
+++ num.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Assign9a
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=5,k=1 ;
+      for(int i=1;i<=n;i++)
+		{
+		  
+		   for (int j=1;j<=i;j++ )
+		   {
+			System.out.print (k + " ");
+			k=k+1;
+		   }
+			System.out.println ();
+
+		  }
+	}
+}
